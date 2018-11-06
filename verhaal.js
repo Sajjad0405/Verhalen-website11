e9b9048b4e6dee6963b53b03e0cd3fede4830553
Sajjad0405 <43657989+Sajjad0405@users.noreply.github.com>
2018-11-06 20:32:58 +0100

    herkansing

--- verhaal.js
+++ verhaal.js
@@ -4,11 +4,15 @@ var firstlink = document.getElementById('first-link');
 
 var paragraph2 = document.querySelector('article.alinea-2');
 
+var ropeIcon = document.querySelector('.rope');
 
-var showParagraph2 = firstlink.addEventListener('click', function(){
+
+var showParagraph2 = firstlink.addEventListener('click', function () {
     paragraph2.classList.toggle('alinea-2-active');
-    
-    console.log(paragraph2);
+})
+
+var showRope = firstlink.addEventListener('click', function () {
+    ropeIcon.classList.toggle('rope-active');
 })
 
 //Weergeef alinea 3
@@ -17,11 +21,15 @@ var secondLink = document.getElementById('second-link');
 
 var paragraph3 = document.querySelector('article.alinea-3');
 
+var leverIcon = document.querySelector('.lever');
 
-var showParagraph3 = secondLink.addEventListener('click', function(){
+
+var showParagraph3 = secondLink.addEventListener('click', function () {
     paragraph3.classList.toggle('alinea-3-active');
-    
-    console.log(paragraph3);
+})
+
+var showLever = secondLink.addEventListener('click', function () {
+    leverIcon.classList.toggle('lever-active');
 })
 
 //Weergeef alinea 4
@@ -31,8 +39,6 @@ var thirdLink = document.getElementById('third-link');
 var paragraph4 = document.querySelector('article.alinea-4');
 
 
-var showParagraph4 = thirdLink.addEventListener('click', function(){
+var showParagraph4 = thirdLink.addEventListener('click', function () {
     paragraph4.classList.toggle('alinea-4-active');
-    
-    console.log(paragraph4);
 })
